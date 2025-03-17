92b5f6510278bf441709f711360514b25147b5ff
nidhisharma2412 <nidhi.sdc@gmail.com>
2025-03-17 13:45:53 +0530

    configured postgresql

---
+++ email_assistant/ai_email/utils.py
@@ -0,0 +1,7 @@
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+def fetch_emails():
+  credentials = service_account.Credentials.from_service_account_file("credentials.json")
+  service = build('gmail', 'v1', credentials=credentials)
+  results = service.users().messages().list(userId='me', maxResults=10).execute()
+  return results.get('messages', [])
--- email_assistant/email_assistant/settings.py
+++ email_assistant/email_assistant/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'email_assistant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'inboxgenie',  
+        'USER': 'postgres', 
+        'PASSWORD': 'password',  
+        'HOST': 'localhost',  
+        'PORT': '5432',  
     }
 }
 
