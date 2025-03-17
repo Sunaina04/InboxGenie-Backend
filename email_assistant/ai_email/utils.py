8ce0729e3ebee0eba8d719228a8814fa66add9ef
Sunaina04 <sunaina.takhi@genboot.com>
2025-03-17 16:20:47 +0530

    migrations applied

--- email_assistant/ai_email/utils.py
+++ email_assistant/ai_email/utils.py
@@ -1,5 +1,6 @@
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+
 def fetch_emails():
   credentials = service_account.Credentials.from_service_account_file("credentials.json")
   service = build('gmail', 'v1', credentials=credentials)
