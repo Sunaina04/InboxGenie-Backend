0919795d365695965a6d8e2cfc8d51ff65a19a30
Sunaina04 <sunaina.takhi@genboot.com>
2025-03-18 11:47:56 +0530

    Cors error fixed

--- email_assistant/email_assistant/settings.py
+++ email_assistant/email_assistant/settings.py
@@ -52,18 +52,29 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
+    "http://127.0.0.1:8080",
     "http://127.0.0.1:3000",
     "http://localhost",
     "http://localhost:3001",
+    "http://localhost:8080",
+    "http://localhost:8000",
     "http://127.0.0.1",
 ]
+
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
+    "http://127.0.0.1:8080",
     "http://127.0.0.1:3000",
     "http://localhost",
     "http://localhost:3000",
+    "http://localhost:8080",
+    "http://localhost:8000",
     "http://127.0.0.1",
 ]
 
